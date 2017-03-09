86b7c24bd24743c2abcbcd16c791c6600be48845
taharushain <taharushain@gmail.com>
2017-03-09 19:06:10 +0500

    Toolbar Demo

---
+++ Codes/AndroidStudio/ToolbarDemo/app/src/main/java/com/example/toolbardemo/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.toolbardemo;
+
+import android.content.Intent;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private Toolbar toolbar;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setTitle("ABCD");
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        if (id == R.id.action_favorite) {
+
+            Snackbar.make(toolbar,"Favorite Action Clicked",Snackbar.LENGTH_SHORT).show();
+
+            return true;
+        }else if(id == R.id.action_settings){
+            Snackbar.make(toolbar, "Settings Action Clicked",Snackbar.LENGTH_SHORT).show();
+
+        }
+
+
+
+        return super.onOptionsItemSelected(item);
+    }
+}
