a3047162d9a793a75e56ee2286b919ad49dbaeaf
taharushain <taharushain@gmail.com>
2017-03-14 15:27:48 +0500

    FragmentDynamic

---
+++ Codes/AndroidStudio/FragmaticDynamic/app/src/main/java/com/golemtron/fragmaticdynamic/QuotesFragment.java
@@ -0,0 +1,52 @@
+package com.golemtron.fragmaticdynamic;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by taharushain on 3/14/17.
+ */
+
+public class QuotesFragment extends Fragment {
+
+    private TextView mQuoteView = null;
+    private int mCurrIdx = -1;
+    private int mQuoteArrayLen;
+
+    int getShownIndex() {
+        return mCurrIdx;
+    }
+
+    // Show the Quote string at position newIndex
+    void showQuoteAtIndex(int newIndex) {
+        if (newIndex < 0 || newIndex >= mQuoteArrayLen)
+            return;
+        mCurrIdx = newIndex;
+        mQuoteView.setText(MainActivity.mQuoteArray[mCurrIdx]);
+    }
+
+    // Called to create the content view for this Fragment
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        // Inflate the layout defined in quote_fragment.xml
+        // The last parameter is false because the returned view does not need to be attached to the container ViewGroup
+        return inflater.inflate(R.layout.quote_fragment, container, false);
+    }
+
+    // Set up some information about the mQuoteView TextView
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mQuoteView = (TextView) getActivity().findViewById(R.id.quoteView);
+        mQuoteArrayLen = MainActivity.mQuoteArray.length;
+    }
+
+
+
+}
---
+++ Codes/AndroidStudio/FragmaticDynamic/app/src/main/java/com/golemtron/fragmaticdynamic/TitlesFragment.java
@@ -0,0 +1,68 @@
+package com.golemtron.fragmaticdynamic;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+/**
+ * Created by taharushain on 3/14/17.
+ */
+
+public class TitlesFragment extends ListFragment {
+
+    private ListSelectionListener mListener = null;
+    private static final String TAG = "TitlesFragment";
+
+    // Callback interface that allows this Fragment to notify the QuoteViewerActivity when
+    // user clicks on a List Item
+    public interface ListSelectionListener {
+        public void onListSelection(int index);
+    }
+
+    // Called when the user selects an item from the List
+    @Override
+    public void onListItemClick(ListView l, View v, int pos, long id) {
+
+        // Indicates the selected item has been checked
+        getListView().setItemChecked(pos, true);
+
+        // Inform the QuoteViewerActivity that the item in position pos has been selected
+        mListener.onListSelection(pos);
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        Log.i(TAG, getClass().getSimpleName() + ":entered onAttach()");
+        super.onAttach(activity);
+
+        try {
+
+            // Set the ListSelectionListener for communicating with the QuoteViewerActivity
+            mListener = (ListSelectionListener) activity;
+
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement OnArticleSelectedListener");
+        }
+    }
+
+
+    @Override
+    public void onActivityCreated(Bundle savedState) {
+        Log.i(TAG, getClass().getSimpleName() + ":entered onActivityCreated()");
+        super.onActivityCreated(savedState);
+
+        // Set the list choice mode to allow only one selection at a time
+        getListView().setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+
+        // Set the list adapter for the ListView
+        // Discussed in more detail in the user interface classes lesson
+        setListAdapter(new ArrayAdapter<String>(getActivity(),
+                R.layout.list_item, MainActivity.mTitleArray));
+    }
+
+}
