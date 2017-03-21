84736f0b8a8aa316d83c9be9eb479295db2e75e6
taharushain <taharushain@gmail.com>
2017-03-21 15:24:29 +0500

    Notifications + BroadcastReceivers

---
+++ Codes/AndroidStudio/NotificationExample/app/src/main/java/com/golemtron/notificationexample/NotificationSubActivity.java
@@ -0,0 +1,15 @@
+package com.golemtron.notificationexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NotificationSubActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification_sub);
+
+
+    }
+}
