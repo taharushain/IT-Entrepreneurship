419da5656751d5159f892662d01077ccb5b7a65e
taharushain <taharushain@gmail.com>
2017-04-18 16:04:37 +0500

    Grades + Attendance

---
+++ Codes/AndroidStudio/PrairieGrainAnalyzers/app/src/main/java/com/golemtron/prairiegrainanalyzers/SplashActivity.java
@@ -0,0 +1,39 @@
+package com.golemtron.prairiegrainanalyzers;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    // Splash screen timer
+    private static int SPLASH_TIME = 1500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(SplashActivity.this, HomeActivity.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME);
+
+
+    }
+}
