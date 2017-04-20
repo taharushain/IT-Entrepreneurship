afdaf98b58d321c329bd6a8a1f7a1d125e271581
taharushain <taharushain@gmail.com>
2017-04-20 12:47:04 +0500

    Attendance, Grades

--- Codes/AndroidStudio/PrairieGrainAnalyzers/app/src/main/java/com/golemtron/prairiegrainanalyzers/GrainItemNameFragment.java
+++ Codes/AndroidStudio/PrairieGrainAnalyzers/app/src/main/java/com/golemtron/prairiegrainanalyzers/GrainItemNameFragment.java
@@ -1,16 +1,23 @@
 package com.golemtron.prairiegrainanalyzers;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.List;
 
 /**
  * Created by taharushain on 4/18/17.
  */
 
-public class GrainItemNameFragment extends ListFragment {
+public class GrainItemNameFragment extends Fragment {
 
 
     public GrainItemNameFragment() {
@@ -21,15 +28,38 @@ public class GrainItemNameFragment extends ListFragment {
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-        String[] grainNames = getResources().getStringArray(R.array.grain_item_names);
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, grainNames);
-        setListAdapter(adapter);
     }
+
+
 
     @Override
-    public void onListItemClick(ListView l, View v, int position, long id) {
-        OnGrainItemNameSelectionChangeListener listener = (OnGrainItemNameSelectionChangeListener) getActivity();
-        listener.OnSelectionChanged(position);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_list, container, false);
+
+        String[] grainNames = getResources().getStringArray(R.array.grain_item_names);
+        ListView lv = (ListView) view.findViewById(R.id.lv_names);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, grainNames);
+//      ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), R.layout.fragment_list_item, R.id.tvName, grainNames);
+        lv.setAdapter(adapter);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                OnGrainItemNameSelectionChangeListener listener = (OnGrainItemNameSelectionChangeListener) getActivity();
+                listener.OnSelectionChanged(position);
+            }
+        });
+
+
+        return view;
     }
+
+
+
+
+
+
+
 }
