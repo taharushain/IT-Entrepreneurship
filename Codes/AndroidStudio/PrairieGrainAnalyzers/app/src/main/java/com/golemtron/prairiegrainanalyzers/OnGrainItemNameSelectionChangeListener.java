0fb21499498abb6acf86cc7ca7bf4280e0423f83
taharushain <taharushain@gmail.com>
2017-04-21 16:16:26 +0500

    fix

--- Codes/AndroidStudio/PrairieGrainAnalyzers/app/src/main/java/com/golemtron/prairiegrainanalyzers/OnGrainItemNameSelectionChangeListener.java
+++
@@ -1,9 +0,0 @@
-package com.golemtron.prairiegrainanalyzers;
-
-/**
- * Created by taharushain on 4/18/17.
- */
-
-public interface OnGrainItemNameSelectionChangeListener {
-    public void OnSelectionChanged(int grainNameIndex);
-}
